bbf95b34d33143ec4d091e09d3b50ec233370ef7
yuizho <yuizho3@gmail.com>
2023-06-05 15:26:12 +0900

    first commit.

---
+++ src/main/java/com/github/yuizho/loganalyticsdemo/LogAnalyticsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.yuizho.loganalyticsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnalyticsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAnalyticsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yuizho/loganalyticsdemo/LogAnalyticsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yuizho.loganalyticsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnalyticsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
